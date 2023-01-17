e70e04aed6afb169e12327503b621ded5d3d0dd2
HunterMRocha <hmrocha2@dons.usfca.edu>
2023-01-16 23:42:36 -0800

    sanity build

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -1,7 +1,8 @@
-import {defineConfig} from 'sanity'
+import {defineCliConfig} from 'sanity/cli'
 
-export default defineConfig({
+export default defineCliConfig({
+  api: {
     projectId: '98tiuus0',
     dataset: 'production'
-
+  }
 })

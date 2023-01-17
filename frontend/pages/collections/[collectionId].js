3502772b08c7131bde09e5faecb79630800856f2
HunterMRocha <hmrocha2@dons.usfca.edu>
2023-01-17 00:09:36 -0800

    project moved to folder

--- frontend/pages/collections/[collectionId].js
+++ frontend/pages/collections/[collectionId].js
@@ -6,7 +6,6 @@ import Link from 'next/link'
 import { useWeb3 } from '@3rdweb/hooks'
 import { client } from '../../lib/sanityClient'
 import { ThirdwebSDK } from '@3rdweb/sdk'
-
 import Header from '../../components/Header'
 import { CgWebsite } from 'react-icons/cg'
 import { AiOutlineInstagram, AiOutlineTwitter } from 'react-icons/ai'
